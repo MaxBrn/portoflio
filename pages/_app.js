49164c7760e0cd3bad796b6568b2e21b0c195aeb
Maxime <maxime_brn@outlook.fr>
2024-11-24 10:38:40 +0100

    Ajout SpeedInsights Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import NavBar from '@/components/NavBar';
 import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 import Header from '@/components/Header';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function MyApp({ Component, pageProps }) {
     return (
@@ -18,6 +19,7 @@ export default function MyApp({ Component, pageProps }) {
         <NavBar/>
         
         <Component {...pageProps} />
+        <SpeedInsights/>
         <Footer />
         </ThemeProvider>
       </>

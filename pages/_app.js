2f23d44d86c087e9d1a2e2b89cbcd0d9c0b09a47
Maxime <maxime_brn@outlook.fr>
2025-02-09 15:25:24 +0100

    Bon là tu vas marcher oui ?

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,6 @@ import '@/app/globals.css';
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
 import Head from 'next/head';
-import { ThemeProvider } from 'next-themes';
-import Header from '@/components/Header';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
@@ -16,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
           <link rel="icon" href="/icon.ico" />
           <meta name="description" content="Bienvenue sur mon portfolio. Découvrez mes compétences et projets en développement web." />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="preload" href="/image/backgroundNew.jpg" as="image" />
+          <link rel="preload" href="/image/background.jpg" as="image" />
         </Head>
         <NavBar/>        
         <Component {...pageProps} />

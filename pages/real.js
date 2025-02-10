262af60a19a440b4e0603965fd636a6ffa5880d9
Maxime <maxime_brn@outlook.fr>
2025-02-10 19:20:58 +0100

    Changement fonctionnement de la section des compétences

--- pages/real.js
+++ pages/real.js
@@ -1,67 +1,33 @@
 import Link from 'next/link';
-import CompetenceModal from '@/components/Competence';
 import { useState, useEffect } from 'react';
 import { IoMdOpen } from "react-icons/io";
 
 export default function Real() {
-  const [isOpen, setIsOpen] = useState(false); // Gère l'ouverture d'une compétence
-  const [files, setFiles] = useState([]); // Fichiers à afficher
-  const [folder, setFolder] = useState(null); // Dossier sélectionné
-  const [preloadedFiles, setPreloadedFiles] = useState({}); // Fichiers préchargés pour chaque dossier
-  const [loading, setLoading] = useState(false); // Indicateur de chargement
+  const [isOpen, setIsOpen] = useState(false);
+  const [files, setFiles] = useState([]);
+  const [folder, setFolder] = useState(null);
+  const [description, setDescription] = useState(null);
 
-  // Fonction pour précharger les fichiers au démarrage
-  const preloadFiles = async () => {
-    const folders = ['1.1', '1.2', '1.3', '1.4', '1.5', '1.6']; // Liste des dossiers à précharger
-    const preloaded = {};
-
-    try {
-      await Promise.all(
-        folders.map(async (folder) => {
-          const response = await fetch(`/api/getFiles?folder=${folder}`);
-          const data = await response.json();
-          preloaded[folder] = data;
-        })
-      );
-      setPreloadedFiles(preloaded); // Met à jour les fichiers préchargés
-    } catch (error) {
-      console.error('Erreur lors du préchargement des fichiers:', error);
-    }
-  };
-
-  // Précharge les fichiers au chargement de la page
   useEffect(() => {
-    preloadFiles();
-  }, []);
-
-  // Fonction pour afficher les fichiers d'une compétence
-  const handleOpenCompetence = async (selectedFolder) => {
-    setFolder(selectedFolder);
-    setLoading(true); // Active le chargement
-
-    if (preloadedFiles[selectedFolder]) {
-      setFiles(preloadedFiles[selectedFolder]);
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
     } else {
-      try {
-        const response = await fetch(`/api/getFiles?folder=${selectedFolder}`);
-        const data = await response.json();
-        setFiles(data);
-        setPreloadedFiles((prev) => ({ ...prev, [selectedFolder]: data })); // Met à jour les fichiers préchargés
-      } catch (error) {
-        console.error(`Erreur de chargement pour le dossier ${selectedFolder}:`, error);
-        setFiles(['Erreur de chargement des fichiers.']);
-      }
+      document.body.style.overflow = 'auto';
     }
+  }, [isOpen]);
 
-    setLoading(false); // Désactive le chargement
+  const handleOpenCompetence = (selectedFolder,description, files) => {
+    setFolder(selectedFolder);
+    setDescription(description);
+    setFiles(files);
     setIsOpen(true);
   };
 
+  const handleClose = () => setIsOpen(false);
+
   return (
-    <div className="pb-10">
-      {/* Section des réalisations */}
+    <div className="relative pb-10">
       <section className="py-28 max-w-7xl mx-auto px-4">
-        {/* Réalisations en stage */}
         <h1 className="text-center text-xl">Les réalisations en stage</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 mt-10">
           <div className="p-6">
@@ -72,7 +38,6 @@ export default function Real() {
           </div>
         </div>
 
-        {/* Réalisations en cours */}
         <p className="text-center mt-20 text-xl">Les réalisations en cours</p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10 mt-10">
           <div className="p-6">
@@ -82,44 +47,77 @@ export default function Real() {
           </div>
         </div>
 
-        {/* Compétences */}
         <p className="mt-20 text-center text-xl flex gap-2 justify-center">Les compétences du cursus <Link href='/competence/synthese.pdf'><IoMdOpen/></Link></p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-10">
-          {[
-            { folder: '1.1', description: 'Gestion du patrimoine informatique' },
-            { folder: '1.2', description: 'Répondre aux incidents et aux demandes d\'assistance et d\'évolution' },
-            { folder: '1.3', description: 'Développer la présence en ligne de l\'organisation' },
-            { folder: '1.4', description: 'Travailler en mode projet' },
-            { folder: '1.5', description: 'Mettre à disposition des utilisateurs un service informatique' },
-            { folder: '1.6', description: 'Organiser son développement personnel' },
-          ].map(({ folder, description }) => (
-            <CompetenceModal
-              key={folder}
-              folder={folder}
-              description={description}
-              onClick={() => handleOpenCompetence(folder)}
-            />
+          {[ 
+            { folder: '1.1', description: 'Gestion du patrimoine informatique', files: 
+              [
+                { name: 'Gestion droits Windows', path: 'gestionDroit.pdf' }, 
+                { name: 'Habilitations GLPI', path: 'habilitationGLPI.pdf' },
+
+              ] 
+            },
+            { folder: '1.2', description: 'Répondre aux incidents et aux demandes d\'assistance et d\'évolution', files: 
+              [
+                { name: 'Collecteur GLPI', path: 'glpiCollecteur.pdf' }, 
+                { name: 'HTTPS-SSL-TSL', path: 'HTTPS-SSL-TSL.pdf' },
+                { name: 'RAID', path: 'RAID.pdf' },
+                { name: 'rsyslog', path: 'rsyslog.pdf' }
+              ] 
+            },
+            { folder: '1.3', description: 'Développer la présence en ligne de l\'organisation', files: 
+              [
+                
+              ] 
+            },
+            { folder: '1.4', description: 'Travailler en mode projet', files: 
+              [
+                
+              ] 
+            },
+            { folder: '1.5', description: 'Mettre à disposition des utilisateurs un service informatique', files: 
+              [
+                { name: 'Installation GLPI', path: 'installGLPI.pdf' },
+              ] 
+            },
+            { folder: '1.6', description: 'Organiser son développement personnel', files: 
+              [
+                { name: 'Formation Unity', path: 'formationUnity.pdf' },
+                { name: 'Certification Azure', path: 'Certif Azure.pdf' },
+                { name: 'Certification Linux', path: 'Certif Linux.png' },
+                { name: 'Commande Linux', path: 'Commande Linux.pdf' },
+                { name: 'MOOC CNIL', path: 'MOOC CNIL.pdf' },
+                { name: 'Certificat PIX', path: 'Pix.pdf' }
+              ] 
+            },
+          ].map(({ folder, description, files }) => (
+            <button key={folder} onClick={() => handleOpenCompetence(folder,description, files)} className="w-full h-full p-4 bg-bgColor2 border border-border rounded-2xl hover:bg-button">
+              <p className="font-bold">Compétence {folder}</p>
+              <p className='pt-2'>{description}</p>
+            </button>
           ))}
         </div>
       </section>
-
-      {/* Modal ou contenu des fichiers */}
+      
       {isOpen && (
-        <div className="modal">
-          <button onClick={() => setIsOpen(false)} className="close-modal">
-            Fermer
-          </button>
-          {loading ? (
-            <p>Chargement des fichiers...</p>
-          ) : (
-            <ul>
-              {files.map((file, index) => (
-                <li key={index}>{file}</li>
-              ))}
-            </ul>
-          )}
-        </div>
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={handleClose}></div>
       )}
+
+      <div className={`p-4 fixed top-0 right-0 h-full md:w-80 w-full bg-bgColor shadow-lg transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-50 text-center`}>
+        <button onClick={handleClose} className="absolute top-4 right-4 text-lg">✖</button>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold">Compétence {folder}</h2>
+          <p className='pt-10'>{description}</p>
+          <div className='my-4 border-t border-b border-border'></div>
+          <ul className="mt-4 flex flex-col gap-2">
+            {files.map(({ name, path }, index) => (
+              <li key={index}>
+                <Link href={`/competence/${folder}/${path}`} target="_blank" className="block p-2 border border-border bg-bgColor2 rounded-2xl hover:bg-button text-center">{name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
